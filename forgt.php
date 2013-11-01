e6fcd75111b86cc95cfaa2673b1b3e680b2d0cc5
kanika <bhatia_kanika@ymail.com>
2013-10-31 18:45:23 -0700

    update

--- forgt.php
+++ forgt.php
@@ -4,16 +4,111 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <title>Best View</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/social-icons.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+       	
+
+        
+        
+        <link rel="Stylesheet" type="text/css" href="js/scroller/css/smoothDivScroll.css" />
+
+        <!--[if IE]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <!-- ENDS JS -->
+
+        <!-- GOOGLE FONTS -->
+        <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+
+
+
     </head>
     <body>
+
+        
+        <!-- Image buttons -->
+        <ul id="image-buttons">
+            <li id="close-image" class="poshytip" title="Close image" ></li>
+            <li id="info-button" class="poshytip" title="Image info"></li>
+        </ul>
+        <!-- ENDS Image buttons -->
+
+        <!-- Navigation -->
+        <div id="nav-wrapper">
+            <ul id="nav" class="sf-menu">
+                <li class="current-menu-item"><a href="index.html">Home</a></li>
+                <li class="current-menu-item"><a href="register.html">Register</a></li>
+                <li class="current-menu-item"><a href="Place_Mark.html">Map</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li class="social">
+                    <!-- Social -->
+                    <a href="http://www.facebook.com" class="poshytip  facebook" title="Become a fan"></a>
+                    <a href="https://twitter.com" class="poshytip  twitter" title="Follow my tweets"></a>
+                    <!-- ENDS Social -->
+                </li>			
+            </ul>
+        </div>
+        <!-- Navigation -->
+        <!-- User Login -->
+        <div>
+            <div class="main1"> 
+                <h2>HI</h2>
+                
+            </div>
+        </div>
+        <!-- JS -->
+        <!-- jQuery library - Please load it from Google API's -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js" ></script>
+
+        <!-- Smoothscroller -->
+        <!-- jQuery UI Widget and Effects Core (custom download)
+             You can make your own at: http://jqueryui.com/download -->
+        <script src="js/scroller/js/jquery-ui-1.8.23.custom.min.js" ></script>
+
+        <!-- Latest version (3.0.6) of jQuery Mouse Wheel by Brandon Aaron
+             You will find it here: http://brandonaaron.net/code/mousewheel/demos -->
+        <script src="js/scroller/js/jquery.mousewheel.min.js" ></script>
+
+        <!-- jQuery Kinectic (1.5) used for touch scrolling -->
+        <script src="js/scroller/js/jquery.kinetic.js" ></script>
+
+        <!-- Smooth Div Scroll 1.3 minified-->
+        <script src="js/scroller/js/jquery.smoothdivscroll-1.3-min.js" ></script>
+        <!-- ENDS Smoothscroller -->
+
+
+        <script src="js/quicksand.js"></script>
+
+        <!-- prettyPhoto -->
+        <script  src="js/prettyPhoto/js/jquery.prettyPhoto.js"></script>
+        <link rel="stylesheet" href="js/prettyPhoto/css/prettyPhoto.css" type="text/css" media="screen" />
+        <!-- ENDS prettyPhoto -->
+
+        <!-- superfish -->
+        <link rel="stylesheet" media="screen" href="css/superfish.css" /> 
+        <script  src="js/superfish-1.4.8/js/hoverIntent.js"></script>
+        <script  src="js/superfish-1.4.8/js/superfish.js"></script>
+        <script  src="js/superfish-1.4.8/js/supersubs.js"></script>
+        <!-- ENDS superfish -->
+
+        <!-- poshytip -->
+        <link rel="stylesheet" href="js/poshytip-1.0/src/tip-twitter/tip-twitter.css" type="text/css" />
+        <link rel="stylesheet" href="js/poshytip-1.0/src/tip-yellowsimple/tip-yellowsimple.css" type="text/css" />
+        <script  src="js/poshytip-1.0/src/jquery.poshytip.min.js"></script>
+        <!-- ENDS poshytip -->
+
+        <script  src="js/backstretch.js"></script>
+        <script  src="js/custom.js"></script>
+        <!-- ENDS JS -->
 
         <?php
     
         //$uname = $_POST["username"];
         $uname = "bhatia_kanika@ymail.com";
-        $page = 'index.html';
         $con = mysql_connect("localhost", "root", "");
         if (!$con) {
             die('Could not connect: ' . mysql_error());
@@ -27,14 +122,8 @@
         else {
         $row = mysql_numrows($result);
         if($row == 1){
-            echo '1';
-            $message = "You have requested for forgot password. ";
-            $message .= "Click the given link to reset your password.";
-            $url = "http://kanikabhatia-photos.com/Team_File_Share/";
-            $message .= $url;
-
-            mail($uname,"Forgot Password",$message);
-            //header("Location: $index.html");
+            echo '1 This is me';
+           
           exit;
         }
         echo '2';
