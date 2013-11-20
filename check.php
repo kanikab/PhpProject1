7bb941fca9a0107663a46703a468da079ca8b85a
kanika <bhatia_kanika@ymail.com>
2013-11-20 15:02:42 -0800

    Version 17

--- check.php
+++ check.php
@@ -5,7 +5,7 @@
     $password = mysql_real_escape_string($_POST['password']);
     
     $result = mysql_query("SELECT * FROM users WHERE email='$email' AND password=md5('$password')");
-
+    echo $result;
     if(mysql_num_rows($result) > 0) {
         $_SESSION["username"]= $email;
         echo hi.$_SESSION["username"];
