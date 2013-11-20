302a72d34eb5eda71241a82828dca0e363ada2b1
kanika <bhatia_kanika@ymail.com>
2013-11-20 15:14:10 -0800

    Version 17

--- check.php
+++ check.php
@@ -5,17 +5,15 @@
     $password = mysql_real_escape_string($_POST['password']);
     
     $result = mysql_query("SELECT * FROM users WHERE email='$email' AND password=md5('$password')");
-    echo $result;
     if(mysql_num_rows($result) > 0) {
+        echo "result".$result['name'];
         $_SESSION["username"]= $email;
-        echo hi.$_SESSION["username"];
-        $_SESSION["name"]= $result['name'];
-        echo bi.$_SESSION["name"];
         //header("location:globe.html");
+        header("location: test.php");
     }
 	else{
                 $_SESSION["logonfail"] = true;
-                echo there.$_SESSION["logonfail"];
+                echo "there".$_SESSION["logonfail"];
 		 //header("location:home.php");
 	}
 ?>
