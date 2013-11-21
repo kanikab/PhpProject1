439cd86c035e92b778aed6cf85a0b87e98acf354
kanika <bhatia_kanika@ymail.com>
2013-11-20 20:34:43 -0800

    21

--- validatefb.php
+++ validatefb.php
@@ -1,4 +1,7 @@
 <?php
+$dir = sys_get_temp_dir();
+session_save_path($dir);
+session_start();
 ob_start();
 include ("rds_db.php");
 include("facebook_constants.php");
@@ -18,6 +21,7 @@ if ($users!="") {
 
 	if(mysql_num_rows($msql)>0){
 		$sqlrow=mysql_fetch_object($msql);
+                $_SESSION['username'] = $femail;
 		header('Location:globe.html'); 
 	}
 	else{		
