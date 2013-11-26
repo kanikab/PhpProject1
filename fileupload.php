bb9329da505a78860f29b9928613095e403194c1
kanika <bhatia_kanika@ymail.com>
2013-11-26 02:03:00 -0800

    version 32

--- fileupload.php
+++ fileupload.php
@@ -49,86 +49,69 @@ ob_start();
            <!-- <form action="page.php" method="post" enctype="multipart/form-data"> -->
                 <h6>Select Location
                     <select id = "place" name="place">
-                        <option value = "Amsterdam">Amsterdam</option>
-                        <option value = "Ankara">Ankara</option>
-                        <option value = "Athens">Athens</option>
-                        <option value = "Atlantic City">Atlantic City</option>
-                        <option value = "Baltimore">Baltimore</option>
-                        <option value = "Bangkok">Bangkok</option>
-                        <option value = "Bangalore">Bangalore</option>
-                        <option value = "Beijing">Beijing</option>
-                        <option value = "Berlin">Berlin</option>
-                        <option value = "Berne">Berne</option>
-                        <option value = "Brussels">Brussels</option>
-                        <option value = "Budapest">Budapest</option>
-                        <option value = "Buenos Aires">Buenos Aires</option>
-                        <option value = "Cairo">Cairo</option>
-                        <option value = "Canberra">Canberra</option> 
-                        <option value = "Cannes">Cannes</option> 
-                        <option value = "Cape Town">Cape Town</option>
-                        <option value = "Chicago">Chicago</option>
-                        <option value = "Cologne">Cologne</option>
-                        <option value = "Copenhagen">Copenhagen</option>
-                        <option value = "Damascus">Damascus</option>
-                        <option value = "Delhi">Delhi</option>
-                        <option value = "Dubai City">Dubai City</option>
-                        <option value = "Dublin">Dublin</option>
-                        <option value = "Florence">Florence</option>
-                        <option value = "Geneva">Geneva</option>
-                        <option value = "Hague">Hague</option>
-                        <option value = "Ha Noi">Ha Noi</option>
-                        <option value = "Havana">Havana</option>
-                        <option value = "Helsinki">Helsinki</option>
-                        <option value = "Hong Kong">Hong Kong</option>
-                        <option value = "Honolulu">Honolulu</option>
-                        <option value = "Istanbul">Istanbul</option> 
-                        <option value = "Jakarta">Jakarta</option>
-                        <option value = "Jerusalem">Jerusalem</option>
-                        <option value = "Kansas City">Kansas City</option>
-                        <option value = "Kathmandu">Kathmandu</option>
-                        <option value = "Kuala Lumpur">Kuala Lumpur</option>
-                        <option value = "Lisbon">Lisbon</option>
-                        <option value = "London">London</option>
-                        <option value = "Los Angeles">Los Angeles</option>
-                        <option value = "Luxembourg">Luxembourg</option>
-                        <option value = "Madrid">Madrid</option>
-                        <option value = "Manila">Manila</option>
-                        <option value = "Melbourne">Melbourne</option>
-                        <option value = "Mexico">Mexico</option>
-                        <option value = "Milan">Milan</option>
-                        <option value = "Montreal">Montreal</option>
-                        <option value = "Moscow">Moscow</option>
-                        <option value = "Mumbai">Mumbai</option>
-                        <option value = "Munich">Munich</option>
-                        <option value = "Nazareth">Nazareth</option>
-                        <option value = "New York">New York</option>
-                        <option value = "Nice">Nice</option>
-                        <option value = "Osaka">Osaka</option>
-                        <option value = "Ottawa">Ottawa</option>
-                        <option value = "Oslo">Oslo</option>
-                        <option value = "Paris">Paris</option>
-                        <option value = "Philadelphia">Philadelphia</option>
-                        <option value = "Phnom Penh">Phnom Penh</option>
-                        <option value = "Prague">Prague</option>
-                        <option value = "Quito">Quito</option>
-                        <option value = "Reykjavik">Reykjavik</option>
-                        <option value = "Rio de Janeiro">Rio de Janeiro</option>
-                        <option value = "San Francisco">San Francisco</option>
-                        <option value = "Santa Fe">Santa Fe</option>
-                        <option value = "Santiago">Santiago</option>
-                        <option value = "Sao Paulo">Sao Paulo</option>
-                        <option value = "Shanghai">Shanghai</option>
-                        <option value = "Singapore">Singapore</option>
-                        <option value = "Stockholm">Stockholm</option>
-                        <option value = "Saint-Petersburg">Saint-Petersburg</option>
-                        <option value = "Sydney">Sydney</option>
-                        <option value = "Taipei">Taipei</option>
-                        <option value = "Tokyo">Tokyo</option>
-                        <option value = "Toronto">Toronto</option>
-                        <option value = "Venice">Venice</option>
-                        <option value = "Vienna">Vienna</option>
-                        <option value = "Washington">Washington</option>
-                        <option value = "Zurich">Zurich</option>
+                        <option value = "Amsterdam|52.3702|4.8952">Amsterdam</option>
+                        <option value = "Athens|37.9837|23.7293">Athens</option>
+                        <option value = "Atlantic City|39.3642|-74.4229">Atlantic City</option>
+                        <option value = "Baltimore|39.2904|-76.6122">Baltimore</option>
+                        <option value = "Bangkok|13.7279|100.5241">Bangkok</option>
+                        <option value = "Bangalore|12.9716|77.5946">Bangalore</option>
+                        <option value = "Beijing|39.9040|116.4075">Beijing</option>
+                        <option value = "Berlin|52.5200|12.4050">Berlin</option>
+                        <option value = "Budapest|47.4979|19.0402">Budapest</option>
+                        <option value = "Cairo|30.0444|31.2357">Cairo</option>
+                        <option value = "Canberra|-35.2820|149.1287">Canberra</option> 
+                        <option value = "Cannes|43.5528|7.0173">Cannes</option> 
+                        <option value = "Chicago|41.8781|-87.6297">Chicago</option>
+                        <option value = "Copenhagen|55.6760|12.5683">Copenhagen</option>
+                        <option value = "Delhi|28.6353|77.2249">Delhi</option>
+                        <option value = "Dubai City|25.2711|55.3075">Dubai City</option>
+                        <option value = "Dublin|53.3498|-6.2603">Dublin</option>
+                        <option value = "Florence|43.7710|11.2480">Florence</option>
+                        <option value = "Geneva|46.1983|6.1423">Geneva</option>
+                        <option value = "Havana|23.1168|-82.3886">Havana</option>
+                        <option value = "Helsinki|60.1733|24.9410">Helsinki</option>
+                        <option value = "Hong Kong|22.3964|114.1095">Hong Kong</option>
+                        <option value = "Honolulu|21.3069|-157.8583">Honolulu</option>
+                        <option value = "Istanbul|41.0052|28.9770">Istanbul</option> 
+                        <option value = "Jakarta|-6.2115|106.8452">Jakarta</option>
+                        <option value = "Jerusalem|31.7683|35.2137">Jerusalem</option>
+                        <option value = "Kansas City|39.0997|-94.5785">Kansas City</option>
+                        <option value = "Lisbon|38.7253|-9.1500">Lisbon</option>
+                        <option value = "London|51.5112|-0.1198">London</option>
+                        <option value = "Los Angeles|34.0522|-118.2436">Los Angeles</option>
+                        <option value = "Luxembourg|49.8152|6.1295">Luxembourg</option>
+                        <option value = "Madrid|40.4168|-3.7037">Madrid</option>
+                        <option value = "Manila|14.5995|120.9842">Manila</option>
+                        <option value = "Melbourne|-37.8141|144.9632">Melbourne</option>
+                        <option value = "Mexico|23.6345|-102.5527">Mexico</option>
+                        <option value = "Milan|45.4654|9.1865">Milan</option>
+                        <option value = "Montreal|45.5086|-73.5539">Montreal</option>
+                        <option value = "Moscow|55.7558|37.6173">Moscow</option>
+                        <option value = "Mumbai|19.0759|72.8776">Mumbai</option>
+                        <option value = "Munich|48.1351|11.5819">Munich</option>
+                        <option value = "New York|40.7143|-74.0059">New York</option>
+                        <option value = "Nice|43.6960|7.2655">Nice</option>
+                        <option value = "Osaka|34.6937|135.5021">Osaka</option>
+                        <option value = "Ottawa|45.4215|-75.6971">Ottawa</option>
+                        <option value = "Oslo|59.9138|10.7522">Oslo</option>
+                        <option value = "Paris|48.8566|2.3522">Paris</option>
+                        <option value = "Philadelphia|39.9523|-75.1637">Philadelphia</option>
+                        <option value = "Prague|50.0755|14.4378">Prague</option>
+                        <option value = "San Francisco|37.7749|-122.4194">San Francisco</option>
+                        <option value = "Santa Fe|35.6869|-105.9377">Santa Fe</option>
+                        <option value = "Santiago|-33.4691|-70.6419">Santiago</option>
+                        <option value = "Sao Paulo|-23.5489|-46.6388">Sao Paulo</option>
+                        <option value = "Shanghai|31.2304|121.4737">Shanghai</option>
+                        <option value = "Singapore|1.3520|103.8198">Singapore</option>
+                        <option value = "Saint-Petersburg|59.9342|30.3350">Saint-Petersburg</option>
+                        <option value = "Sydney|-33.8674|151.2069">Sydney</option>
+                        <option value = "Taipei|25.0910|121.5598">Taipei</option>
+                        <option value = "Tokyo|35.6894|139.6917">Tokyo</option>
+                        <option value = "Toronto|43.6532|-79.3831">Toronto</option>
+                        <option value = "Venice|45.4408|12.3155">Venice</option>
+                        <option value = "Vienna|48.2081|16.3738">Vienna</option>
+                        <option value = "Washington|38.9072|-77.0364">Washington</option>
+                        <option value = "Zurich|47.3686|8.5391">Zurich</option>
                     </select>
                     <br><br>
                     <input name="file" type="file" class="offset2"/><br><br>
@@ -165,10 +148,12 @@ ob_start();
                             if ($_FILES["file"]["error"] > 0) {
                                 echo "Error: " . $_FILES["file"]["error"] . "<br>";
                             } else {
-                                $username = $_SESSION['username'];
+                                //$username = $_SESSION['username'];
+                                $username = "kanikabhtia@gmail.com";
                                 $bucket = 'bestview-bucket';
-                                $loc = $_POST['place'];
-                                $filename = $loc . "____" . $_FILES["file"]["name"];
+                                $location = $_POST['place'];
+                                $loc = explode('|', $location);
+                                $filename = $loc[0] . "____" . $_FILES["file"]["name"];
                                 $type = $_FILES["file"]["type"];
                                 $size = $_FILES["file"]["size"];
                                 $sizebytes = ( $size / 1024) . "kB";
@@ -180,15 +165,14 @@ ob_start();
                                 $fp = fopen($tmp, 'r');
                                 $content = fread($fp, $size);
                                 $content = addslashes($content);
-
                                 $fileData = file_get_contents($tmp);
                                 $fhandle = fopen($filename, 'w') or die("Error opening file");
                                 fwrite($fhandle, $fileData) or die("Error writing to file");
                                 fclose($fhandle) or die("Error closing file");
                                 fclose($fp);
-                                $sql = "INSERT into content VALUES ('" . $loc . "','" . $fname . "','" . $date . "','" . $content . "','" . $type . "')";
+                                $sql = "INSERT into content VALUES ('" . $loc[0] . "','" . $fname . "','" . $date . "','" . $content . "','" . $type . "')";
                                 if (!mysql_query($sql)) {
-                                    die('Error: ' . mysql_error());
+                                    die('Error: ' . mysql_error());      
                                 } 
 
                                 //create a new bucket
@@ -197,6 +181,7 @@ ob_start();
                                     echo "<script type='text/javascript'>
                                         alert(\"File Uploaded \");
                                     </script>";
+                                    placemark($location);
                                 } else {
                                     echo "<script type='text/javascript'>
                                         alert(\"Something went wrong while uploading your file... sorry. \");
@@ -205,5 +190,23 @@ ob_start();
                             }
                         }
 
-                    
+function placemark($result)
+{
+    if (isset($result)) {
+    $result_explode = explode('|', $result);
+    $lat = $result_explode[1];
+    $lon = $result_explode[2];
+    $city= $result_explode[0];
+    include_once("rds_db.php");
+
+    $sql = "SELECT * FROM globe WHERE name = '$city'";
+    $res = mysql_query($sql);
+    $row = mysql_fetch_array($res);
+
+    if(!$row) {
+        $sql = "INSERT INTO globe (latitude,longitude, name) VALUES ('$lat', '$lon', '$city')";
+        mysql_query($sql);
+    }
+}
+}
                     ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $dir = sys_get_temp_dir();
 session_save_path($dir);
 session_start();
@@ -6,94 +6,111 @@ session_start();
 <!doctype html>
 
 <head>
-	
+
     <!-- General Metas -->
     <meta charset="utf-8" />
     <meta name="google-translate-customization" content="dd6dc8d31eae27c9-1af41dda936d789b-g8a2fe6bb665c9411-1d"></meta>
 
     <title>Citystory</title>
 
-  	<!-- Stylesheets -->
-	<link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.css">
-	<style>
-	html,body { 
-	  background: url(images/login.jpg) no-repeat center center fixed; 
-	  -webkit-background-size: cover;
-	  -moz-background-size: cover;
-	  -o-background-size: cover;
-	  background-size: cover;
-	}
-	.container {
-	     padding-right: 0; /*15px in bootstrap.css*/
-	     padding-left: 0;  /*idem*/
-	     margin-right: auto;
-	     margin-left: auto;
-	}
-	</style>
+    <!-- Stylesheets -->
+    <link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <style>
+        html,body { 
+            background: url(images/login.jpg) no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .container {
+            padding-right: 0; /*15px in bootstrap.css*/
+            padding-left: 0;  /*idem*/
+            margin-right: auto;
+            margin-left: auto;
+        }
+        .google_translate{
+            display: block;
+            top: 5px;
+            float: right;    
+            background: #fdfdfd;
+            border: 1px solid #ccc;
+
+        }
+    </style>
 
 </head>
 <body>
-      <script src="bootstrap/js/bootstrap.js"></script>
-		&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;
-	<div class="container-fluid">
- 	<form class="well span5 pull-right" action="check.php" method="POST">
-		<div>
-	        <p style="text-align:center; margin-top:0px; margin-bottom:0px; padding:0px;">
-	            <img src="images/logo.jpeg" alt="citystory" width=150 height=150/><br>
-	            Time travel through images
-	        </p>    
-	    </div>
-		<br><br><label>Username</label>
-		<input type="email" class="span3" autofocus="autofocus" placeholder="example@xyz.com" name="email" required/> </br>
-		<label>Password </label>
-		<input type="password" class="span3" placeholder="password" name="password" required/></br>
-		<button  type="submit" value="submit" class="btn btn-primary">Submit </button>
-		<button type="reset" class="btn">Clear</button></br></br>
-		<input type="checkbox" id="remember" value="remember" />
-        <span>Remember me on this computer</span></br>
- 		<div id="text" style="color: red" hidden ="true"> Enter valid login details </div></br><br>
-		 <!-- Facebook Login -->
-		   <div id="fb-root" style="float:left; width:1px;"></div>
+    <div id="google_translate_element" class="google_translate"></div>
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+            }
+        </script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    &nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;
+
+    <div class="container-fluid">
+        <form class="well span5 pull-right" action="check.php" method="POST">
+            <div>
+                <p style="text-align:center; margin-top:0px; margin-bottom:0px; padding:0px;">
+                    <img src="images/logo.jpeg" alt="citystory" width=150 height=150/><br>
+                    Time travel through images
+                </p>    
+            </div>
+            <br><br><label>Username</label>
+            <input type="email" class="span3" autofocus="autofocus" placeholder="example@xyz.com" name="email" required/> </br>
+            <label>Password </label>
+            <input type="password" class="span3" placeholder="password" name="password" required/></br>
+            <button  type="submit" value="submit" class="btn btn-primary">Submit </button>
+            <button type="reset" class="btn">Clear</button></br></br>
+            <input type="checkbox" id="remember" value="remember" />
+            <span>Remember me on this computer</span></br>
+            <div id="text" style="color: red" hidden ="true"> Enter valid login details </div></br><br>
+            <!-- Facebook Login -->
+            <div id="fb-root" style="float:left; width:1px;"></div>
 
 
-		<script>
-		window.fbAsyncInit = function() {
-		    FB.init({
-		       appId: '671161372917644',
-		       cookie: true,
-		       xfbml: true,
-		       oauth: true
-		    }); 
-		};
+            <script>
+                window.fbAsyncInit = function() {
+                    FB.init({
+                        appId: '671161372917644',
+                        cookie: true,
+                        xfbml: true,
+                        oauth: true
+                    });
+                };
 
-		(function() {
-		var e = document.createElement('script'); e.async = true;
-		e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-		document.getElementById('fb-root').appendChild(e);
-		}());
+                (function() {
+                    var e = document.createElement('script');
+                    e.async = true;
+                    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+                    document.getElementById('fb-root').appendChild(e);
+                }());
 
-		function fblogin(){
-		FB.login(function(response){
-		if (response.authResponse) {
-		window.location='validatefb.php';
-		}
-		},{scope: 'publish_stream'});
-		}
-		</script>  
-		<p style="text-align:left; margin-top:0px; margin-bottom:0px; padding:0px;">
-		 <img border="0" src="login_facebook.png" onClick="fblogin();" border="0" 
-		style="cursor:pointer">
-            <a href="register.php"><img src="rainbow.gif"></a></br></br>
-        <p class="forgot">Forgot your password? <a href="forgot.php"> Reset</a></p>
-        <p class="forgot">New user? <a href="register.php">Register</a></p>
-		</form>
-		</div>
-		<!-- JS  -->
-	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
-	    <script>window.jQuery || document.write("<script src='js/jquery-1.5.1.min.js'>\x3C/script>")</script>
-	    <script src="js/app.js"></script>
-		
+                function fblogin() {
+                    FB.login(function(response) {
+                        if (response.authResponse) {
+                            window.location = 'validatefb.php';
+                        }
+                    }, {scope: 'publish_stream'});
+                }
+            </script>  
+            <p style="text-align:left; margin-top:0px; margin-bottom:0px; padding:0px;">
+                <img border="0" src="login_facebook.png" onClick="fblogin();" border="0" 
+                     style="cursor:pointer">
+                <a href="register.php"><img src="rainbow.gif"></a></br></br>
+            <p class="forgot">Forgot your password? <a href="forgot.php"> Reset</a></p>
+            <p class="forgot">New user? <a href="register.php">Register</a></p>
+        </form>
+    </div>
+    <!-- JS  -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+    <script>window.jQuery || document.write("<script src='js/jquery-1.5.1.min.js'>\x3C/script>")</script>
+    <script src="js/app.js"></script>
 
-        <!-- End Document -->
-                    </body>
-                    </html>
+
+    <!-- End Document -->
+</body>
+</html>
