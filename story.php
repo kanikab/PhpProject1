228a8730dfa9b211510e44d3754eadfc9f051b79
doraemon-shasha <chenshasha.mail@gmail.com>
2013-11-27 16:09:48 -0800

    remove the audio

--- story.php
+++ story.php
@@ -68,32 +68,7 @@ session_start();
                 });
             });
         </script>
-        <div id = "audio" class = "audio_control"> </div>
- 	<!-- audio control -->
-	<link rel="stylesheet" type="text/css" href="./plugin/css/style.css">
-    	<link rel="stylesheet" type="text/css" href="css/audio.css">
-    	<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
-    	<script type="text/javascript" src="./plugin/jquery-jplayer/jquery.jplayer.js"></script>
-    	<script type="text/javascript" src="./plugin/ttw-music-player-min.js"></script>
-    	<script type="text/javascript" src="js/myplaylist.js"></script>
-    	<script type="text/javascript">
-        $("audio").ready(function(){
-            
-            $('.audio_control').ttwMusicPlayer(myPlaylist, {
-                autoplay:true, 
-                jPlayer:{
-                    swfPath:'./plugin/jquery-jplayer' //You need to override the default swf path any time the directory structure changes
-                }
-            });
-        });
-    	</script>
-	<style>
-  		#audio {
-    		position: fixed;
-    		top:30px;
-    		left: 0;
-  		}
-	</style>
+        
 
 
 
