2f0b866f8f275c7ff412049827ce7c6dada4e931
doraemon-shasha <chenshasha.mail@gmail.com>
2013-11-30 23:58:56 -0800

    debug

--- index.php
+++ index.php
@@ -108,7 +108,6 @@ session_start();
     <!-- JS  -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
     <script>window.jQuery || document.write("<script src='js/jquery-1.5.1.min.js'>\x3C/script>")</script>
-    <script src="js/app.js"></script>
 
 
     <!-- End Document -->
