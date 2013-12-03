2263383bd7973c29966caa40a1b8bd7f589daf79
doraemon-shasha <chenshasha.mail@gmail.com>
2013-12-03 14:58:22 -0800

    music

--- js/myplaylist.js
+++ js/myplaylist.js
@@ -6,31 +6,31 @@
  * Time: 9:39 AM
  */
 
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
 var myPlaylist = [
 
     {
-        mp3:'mix/1.mp3',
-        oga:'mix/1.ogg',
-        title:'Sample',
-        artist:'Sample',
-        rating:4,
-        duration:'0:30',
-        cover:'mix/1.png'
+            mp3:'mix/UnderTheBridge.mp3',
+            title:'Under The Bridge',
+            artist:'Under The Bridge',
+            rating:4,
+            duration:'0:30',
+            cover:'mix/UnderTheBridge.jpg'
     },
     {
-        mp3:'mix/UnderTheBridge.mp3',
-        title:'Under the Bridge',
-        artist:'Under the Bridge',
-        rating:4,
-        duration:'4:05',
-        cover:'mix/UnderTheBridge.jpg'
-    },
-    {
-        mp3:'mix/End_Of_May.mp3',
-        title:'End of May',
-        artist:'End of May',
-        rating:4,
-        duration:'1:48',
-        cover:'mix/End_Of_May.jpg'
-    }
+            mp3:'mix/End_Of_May.mp3',
+            title:'End Of May',
+            artist:'End Of May',
+            rating:4,
+            duration:'4:05',
+            cover:'mix/End_Of_May.jpg'
+     }
 ];
+
